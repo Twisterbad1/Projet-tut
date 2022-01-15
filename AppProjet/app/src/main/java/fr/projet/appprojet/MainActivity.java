178ef1c734a31f542fbbf766eb13f91a2f38e882
paulISN <paul.imsaroeun@gmail.com>
2022-01-15 17:25:33 +0100

    Application Orthèse

---
+++ AppProjet/app/src/main/java/fr/projet/appprojet/MainActivity.java
@@ -0,0 +1,30 @@
+package fr.projet.appprojet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView start;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.start = (ImageView) findViewById(R.id.start);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent otherActivity = new Intent(getApplicationContext(), MainActivity2.class);
+                startActivity(otherActivity);
+                finish();
+            }
+        });
+    }
+}
